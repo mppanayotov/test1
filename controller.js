3b7d9e7cacbdd1ccad76040744a33ef472ef309c
milenpanayotov-2c <milenpanayotov@2create.studio>
2020-10-19 17:17:05 +0300

    Update form functions

--- controller.js
+++ controller.js
@@ -1,8 +1,21 @@
 app.controller('myCtrl', function($scope, $http) {
 	$scope.name = 'Angular ';
-
-	$http.get('users.json').success(function (res){
+	users = [];
+	$http.get('https://raw.githubusercontent.com/mppanayotov/test1/master/assets/json/users.json').success(function(res) {
 		$scope.users = res;
 		console.table($scope.users);
 	});
+	$scope.sort = {
+		column: '',
+		descending: false
+	};
+	$scope.changeSorting = function(column) {
+		var sort = $scope.sort;
+		if (sort.column == column) {
+			sort.descending = !sort.descending;
+		} else {
+			sort.column = column;
+			sort.descending = false;
+		}
+	};
 });
